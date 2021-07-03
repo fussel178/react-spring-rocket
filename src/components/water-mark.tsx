6cf0f0e00e73ee2da64797fddf99ed90e0c69ae7
Ludwig Richter <riluzm@posteo.de>
2021-07-03 13:20:49 +0200

    feat: Add water mark

--- src/components/water-mark.tsx
+++ src/components/water-mark.tsx
@@ -7,6 +7,29 @@ export function WaterMark() {
 			<div>
 				<p>Part of Telestion</p>
 				<p>A project by WüSpace e. V.</p>
+				<p>
+					Rocket image by
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://therealjerrylow.com/"
+					>
+						Jerry Low
+					</a>
+				</p>
+				{/*<a rel="license" href="http://creativecommons.org/licenses/by/3.0/">
+					<img
+						alt="Creative Commons License"
+						style={{ borderWidth: 0 }}
+						src="https://i.creativecommons.org/l/by/3.0/80x15.png"
+					/>
+				</a>
+				<br />
+				This work is licensed under a{' '}
+				<a rel="license" href="http://creativecommons.org/licenses/by/3.0/">
+					Creative Commons Attribution 3.0 Unported License
+				</a>
+				.*/}
 			</div>
 			<div>
 				<img src={Logo} alt="WüSpace e. V. Logo" />
